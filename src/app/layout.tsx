482518022481e1adaabb2a72c0ebee01e1055abb
edy <edy@apyhub.com>
2023-02-18 11:05:54 +0700

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body className="px-4">
+        <nav className="py-4">My Blog</nav>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+}
